4bee2df228bcce1ff8a55b7659259dcf70217cd6
sima666 <sima666@users.noreply.github.com>
2018-02-26 15:59:41 +0800

    add some resources for web and some configuration files

---
+++ myplatform-web/src/main/java/com/lovemao/myplatform/web/protal/LoginController.java
@@ -0,0 +1,26 @@
+package com.lovemao.myplatform.web.protal;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 
+ * @Description <br>
+ * @Author MaoC <br>
+ * @Since 2018年2月26日下午2:25:13 <br>
+ *
+ */
+@Controller
+public class LoginController {
+	private Logger logger = Logger.getLogger(LoginController.class);
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
+		logger.info("===========login start===========");
+
+		logger.info("===========login Enndd===========");
+		return "login";
+	}
+}
