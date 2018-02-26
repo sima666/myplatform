96625ce8b85406af767ff97e3887a8942af28284
sima666 <sima666@users.noreply.github.com>
2018-02-26 16:21:00 +0800

    login html page

---
+++ myplatform-web/src/main/java/com/lovemao/myplatform/ApplicationBootstrap.java
@@ -0,0 +1,20 @@
+package com.lovemao.myplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @Description <br>
+ * @Author MaoC <br>
+ * @Since 2018年2月26日下午4:05:26 <br>
+ *
+ */
+@SpringBootApplication
+public class ApplicationBootstrap {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationBootstrap.class, args);
+	}
+
+}
